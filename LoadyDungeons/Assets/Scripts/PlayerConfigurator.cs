1dd772e5054728793492c3bf6c707fa63b93a9a2
ww-org <semir@shadowwhite.org>
2024-09-14 18:17:51 +0300

    Update Addressable Assets and player logic
    - Updated `AddressableAssetSettings.asset` with new profiles and active profile ID.
    - Modified `Default Local Group.asset` to change hat asset address.
    - Refactored `PlayerConfigurator.cs` to use Addressable Assets for hat loading and added reloading logic.
    - Added `ProfileDataSourceSettings.asset` for profile group types and variables.
    - Introduced `ProfileVariableTester.cs` for testing Addressable Assets profile variables.
    - Added `ScriptableBuildPipeline.json` for build pipeline configuration.

--- LoadyDungeons/Assets/Scripts/PlayerConfigurator.cs
+++ LoadyDungeons/Assets/Scripts/PlayerConfigurator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
@@ -20,40 +19,67 @@ public class PlayerConfigurator : MonoBehaviour
     //[SerializeField]
     //private AssetReference m_HatAssetReference;
 
-    [SerializeField] AssetReferenceGameObject m_HatAssetReference;
+    //[SerializeField] 
+    //AssetReferenceGameObject m_HatAssetReference;
+
+    private GameObject m_HatInstance;
 
 
     private AsyncOperationHandle<GameObject> m_HatLoadOpHandle;
 
     void Start()
-    {           
-        SetHat(string.Format("Hat{0:00}", GameManager.s_ActiveHat));
-    }
-
-    public void SetHat(string hatKey)
     {
-        /// we will change the following lines to replace the resource loading system with the addressable system
-        //m_HatLoadingRequest = Resources.LoadAsync(hatKey);
-        //m_HatLoadingRequest.completed += OnHatLoaded;
-
-        // due to m_address variable has removed, this line is obsolute
-        //m_HatLoadOpHandle = Addressables.LoadAssetAsync<GameObject>(m_Address);
-
-        if (!m_HatAssetReference.RuntimeKeyIsValid())
-            return;
-
-        m_HatLoadOpHandle = m_HatAssetReference.LoadAssetAsync<GameObject>();
-
-        m_HatLoadOpHandle.Completed += OnHatLoadComplete;
-
+        //SetHat(string.Format("Hat{0:00}", GameManager.s_ActiveHat));
+        LoadInRandomHat();
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(1))
+        {
+            Destroy(m_HatInstance);
+            Addressables.ReleaseInstance(m_HatLoadOpHandle);
+
+            LoadInRandomHat();
+        }
+    }
+
+
+    private void LoadInRandomHat()
+    {
+        int randomIndex = Random.Range(0, 6);
+        string hatAddress = string.Format("Hat{0:00}", randomIndex);
+
+        m_HatLoadOpHandle = Addressables.LoadAssetAsync<GameObject>(hatAddress);
+        m_HatLoadOpHandle.Completed += OnHatLoadComplete;
+    }
+
+    //public void SetHat(string hatKey)
+    //{
+    //    /// we will change the following lines to replace the resource loading system with the addressable system
+    //    //m_HatLoadingRequest = Resources.LoadAsync(hatKey);
+    //    //m_HatLoadingRequest.completed += OnHatLoaded;
+
+    //    // due to m_address variable has removed, this line is obsolute
+    //    //m_HatLoadOpHandle = Addressables.LoadAssetAsync<GameObject>(m_Address);
+
+    //    if (!m_HatAssetReference.RuntimeKeyIsValid())
+    //        return;
+
+    //    m_HatLoadOpHandle = m_HatAssetReference.LoadAssetAsync<GameObject>();
+
+    //    m_HatLoadOpHandle.Completed += OnHatLoadComplete;
+
+    //}
 
     private void OnHatLoadComplete(AsyncOperationHandle<GameObject> handle)
     {
         Debug.Log($"Async Operation Handle Status; "+ handle.Status);
 
-        if (handle.Status == AsyncOperationStatus.Succeeded)
-            Instantiate(handle.Result, m_HatAnchor);
+        //if (handle.Status == AsyncOperationStatus.Succeeded)
+        //    Instantiate(handle.Result, m_HatAnchor);
+
+        m_HatInstance = Instantiate(handle.Result, m_HatAnchor);
 
 
     }
