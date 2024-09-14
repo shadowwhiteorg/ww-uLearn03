750eac60f29be67375899554761dfcc770bd7146
ww-org <semir@shadowwhite.org>
2024-09-14 13:06:52 +0300

    Update Unity components and switch to Addressable Assets
    Updated serialized versions and properties for various Unity
    components in Level_00.unity to enhance functionality and
    compatibility. Switched from Resources to Addressable Assets
    system for loading hat assets, added necessary using directives,
    introduced new serialized fields, and modified methods to
    accommodate the new loading system.

--- LoadyDungeons/Assets/Scripts/PlayerConfigurator.cs
+++ LoadyDungeons/Assets/Scripts/PlayerConfigurator.cs
@@ -1,12 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 // Used for the Hat selection logic
 public class PlayerConfigurator : MonoBehaviour
 {
+
     [SerializeField]
     private Transform m_HatAnchor;
 
     private ResourceRequest m_HatLoadingRequest;
+
+    [SerializeField]
+    private string m_Address;
+
+    private AsyncOperationHandle<GameObject> m_HatLoadOpHandle;
 
     void Start()
     {           
@@ -15,8 +24,23 @@ public class PlayerConfigurator : MonoBehaviour
 
     public void SetHat(string hatKey)
     {
-        m_HatLoadingRequest = Resources.LoadAsync(hatKey);
-        m_HatLoadingRequest.completed += OnHatLoaded;
+        /// we will change the following lines to replace the resource loading system with the addressable system
+        //m_HatLoadingRequest = Resources.LoadAsync(hatKey);
+        //m_HatLoadingRequest.completed += OnHatLoaded;
+
+        m_HatLoadOpHandle = Addressables.LoadAssetAsync<GameObject>(m_Address);
+        m_HatLoadOpHandle.Completed += OnHatLoadComplete;
+
+    }
+
+    private void OnHatLoadComplete(AsyncOperationHandle<GameObject> handle)
+    {
+        Debug.Log($"Async Operation Handle Status; "+ handle.Status);
+
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+            Instantiate(handle.Result, m_HatAnchor);
+
+
     }
 
     private void OnHatLoaded(AsyncOperation asyncOperation)
@@ -26,7 +50,10 @@ public class PlayerConfigurator : MonoBehaviour
 
     private void OnDisable()
     {
-        if (m_HatLoadingRequest != null)
-            m_HatLoadingRequest.completed -= OnHatLoaded;
+        //if (m_HatLoadingRequest != null)
+        //    m_HatLoadingRequest.completed -= OnHatLoaded;
+
+        m_HatLoadOpHandle.Completed -= OnHatLoadComplete;
+
     }
 }
